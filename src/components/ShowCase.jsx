fa9270d6a633e00b983256961ee2e991b8e722b8
imran122 <imransardar122@gmail.com>
2024-03-06 14:25:35 +0600

    categorey done

--- src/components/ShowCase.jsx
+++ src/components/ShowCase.jsx
@@ -1,3 +1,5 @@
+import { LazyMotion, domAnimation, motion } from "framer-motion";
+import { useState } from "react";
 import image from "../assets/images/chef/Image.png";
 import image2 from "../assets/images/chef/Image2.png";
 import image3 from "../assets/images/chef/Image3.png";
@@ -5,6 +7,34 @@ import image4 from "../assets/images/chef/Image4.png";
 import image5 from "../assets/images/chef/Image5.png";
 import arrow from "../assets/images/mid-section/line.png";
 export default function ShowCase() {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [numImages, setNumImages] = useState(2); // Initially show 2 images
+
+  // Function to handle category selection
+  const handleCategorySelect = (category) => {
+    setSelectedCategory(category);
+    setNumImages(2); // Reset number of images to display to 2
+  };
+
+  // Filter images based on selected category
+  const filteredImages = () => {
+    switch (selectedCategory) {
+      case "All":
+        return [image, image2, image3, image4, image5];
+      case "Burger":
+        return [image, image2];
+      case "Drinks":
+        return [image2, image3];
+      case "Pizza":
+        return [image, image3, image4];
+      case "Dinner":
+        return [image4, image5];
+      case "Lunch":
+        return [image5, image, image2];
+      default:
+        return [];
+    }
+  };
   return (
     <div className="container mx-auto px-2 py-10">
       <div className="flex flex-col gap-3 justify-center items-center py-4 px-2">
@@ -24,35 +54,73 @@ export default function ShowCase() {
 
       <div>
         <ul className="flex flex-wrap gap-6 justify-center items-center py-4">
-          <li className="border border-secondary_colour rounded-full py-3 px-3 text-secondary_text">
+          <li
+            className="border border-secondary_colour rounded-full py-3 px-3 text-secondary_text cursor-pointer"
+            onClick={() => handleCategorySelect("All")}
+          >
             All
           </li>
-          <li>Burger</li>
-          <li>Drinks</li>
-          <li>Pizza</li>
-          <li>Dinner</li>
-          <li>Lunch</li>
-          <li>Cookies</li>
-          <li>Backey</li>
+          {/* Add onClick handlers to other category items */}
+          {/* For example: */}
+          <li
+            className="cursor-pointer"
+            onClick={() => handleCategorySelect("Burger")}
+          >
+            Burger
+          </li>
+          <li
+            className="cursor-pointer"
+            onClick={() => handleCategorySelect("Drinks")}
+          >
+            Drinks
+          </li>
+          <li
+            className="cursor-pointer"
+            onClick={() => handleCategorySelect("Pizza")}
+          >
+            Pizza
+          </li>
+          <li
+            className="cursor-pointer"
+            onClick={() => handleCategorySelect("Dinner")}
+          >
+            Dinner
+          </li>
+          <li
+            className="cursor-pointer"
+            onClick={() => handleCategorySelect("Lunch")}
+          >
+            Lunch
+          </li>
         </ul>
         {/*  image section design */}
-        <div className="grid grid-cols-12 gap-6 py-4">
-          <div className="col-span-4">
-            <img className="w-full h-80" src={image} />
+        <LazyMotion features={domAnimation}>
+          <div className="grid grid-cols-12 gap-6 py-4">
+            {filteredImages().map((img, index) => (
+              <div
+                key={index}
+                className={`${index === 0 && "col-span-4"} 
+              ${index === 1 && "col-span-8"} 
+                ${index === 2 && "col-span-3"} 
+                ${index === 3 && "col-span-5"} 
+              ${index === 4 && "col-span-4"}`}
+              >
+                <motion.img
+                  initial={{ scale: 0 }}
+                  animate={{ scale: 1, opacity: 1 }}
+                  transition={{
+                    type: "spring",
+                    stiffness: index + 40,
+                    damping: index + 20,
+                  }}
+                  className="w-full h-80"
+                  src={img}
+                  alt={`Image ${index + 1}`}
+                />
+              </div>
+            ))}
           </div>
-          <div className="col-span-8">
-            <img className="w-full h-80" src={image2} />
-          </div>
-          <div className="col-span-3">
-            <img className="w-full h-80" src={image3} />
-          </div>
-          <div className="col-span-5">
-            <img className="w-full h-80" src={image4} />
-          </div>
-          <div className="col-span-4">
-            <img className="w-full h-80" src={image5} />
-          </div>
-        </div>
+        </LazyMotion>
       </div>
     </div>
   );
